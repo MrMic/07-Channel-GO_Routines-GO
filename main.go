9c8203be55176f3d4c184c9833e830a02f89211d
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-04-03 13:07:51 +0200

    feat(UX): Channel Implementation

--- main.go
+++ main.go
@@ -1,3 +1,37 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+)
+
+// * INFO: ══ MAIN ════════════════════════════════════════════════════════════
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	fmt.Println(<-c)
+}
+
+// ______________________________________________________________________
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down")
+		c <- "Might be DOWN I think"
+		return
+	}
+
+	fmt.Println(link, "is up!")
+	c <- "Yep, it's UP!"
+}
